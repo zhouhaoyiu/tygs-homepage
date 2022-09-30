b4090a19e41756f7f9ab3b614c5c31910b74fe18
云香绕雪 <67226385+zhouhaoyiu@users.noreply.github.com>
2022-09-30 12:27:26 +0800

    clean

--- src/CustomerService.tsx
+++ src/CustomerService.tsx
@@ -6,33 +6,10 @@ const CustomerService = (): JSX.Element => {
 
   React.useEffect(() => {
     // className为count的在更新时重新渲染
-
-    document.querySelector(".count")?.animate(
-      [
-        // keyframes
-        { transform: "translateY(0)" },
-        { transform: "translateY(-100%)" },
-        { transform: "translateY(0)" },
-      ],
-      {
-        duration: 1000, // timing options
-        iterations: Infinity,
-      }
-    );
   }, [count]);
   return (
     <div className="animate__animated animate__zoomIn">
       <h1>Customer Service</h1>
-      <div className="count">{count}</div>
-      <button className="button" onClick={() => setCount((count += 1))}>
-        count ++{" "}
-      </button>
-      <button className="button" onClick={() => setCount((count -= 1))}>
-        count --{" "}
-      </button>
-      <button className="button" onClick={() => setCount((count = 0))}>
-        count = 0{" "}
-      </button>
     </div>
   );
 };
